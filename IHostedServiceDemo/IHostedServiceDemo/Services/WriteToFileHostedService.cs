c3184597f72154549089b2d94121d6112aee74c9
HegaRo <henrygarciaospina@gmail.com>
2020-07-19 17:36:07 -0500

    Construyendo Web APIs RESTful con ASP.NET Core 3.1 Vídeo 35 - Ejecutar Código recurrente con IHostedService Ejemplo 1

---
+++ IHostedServiceDemo/IHostedServiceDemo/Services/WriteToFileHostedService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class WriteToFileHostedService : IHostedService
+    {
+        private readonly IHostEnvironment environment;
+        private readonly string fileName = "File 1.txt";
+
+        public WriteToFileHostedService(IHostEnvironment environment) 
+        {
+            this.environment = environment;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService : Process Started " + DateTime.Now.ToString("H:mm:ss"));
+            return Task.CompletedTask;
+        }
+
+        private void WriteToFile(string message)
+        {
+            var path = $@"{environment.ContentRootPath}\wwwroot\{fileName}";
+            using (StreamWriter writer = new StreamWriter(path, append: true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService : Process Stopped " + DateTime.Now.ToString("H:mm:ss"));
+            return Task.CompletedTask;
+        }
+    }
+}
