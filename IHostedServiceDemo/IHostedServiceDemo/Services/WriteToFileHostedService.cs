1390a4e557844e52e44d9e3ed8e999124a132ea4
HegaRo <henrygarciaospina@gmail.com>
2020-07-19 18:20:51 -0500

    Construyendo Web APIs RESTful con ASP.NET Core 3.1 35. Ejecutar Código Recurrente con IHostedService Ejemplo 2

--- IHostedServiceDemo/IHostedServiceDemo/Services/WriteToFileHostedService.cs
+++ IHostedServiceDemo/IHostedServiceDemo/Services/WriteToFileHostedService.cs
@@ -9,18 +9,19 @@ using System.Threading.Tasks;
 
 namespace IHostedServiceDemo.Services
 {
-    public class WriteToFileHostedService : IHostedService
+    public class WriteToFileHostedService : IHostedService, IDisposable
     {
         private readonly IHostEnvironment environment;
         private readonly string fileName = "File 1.txt";
-
+        private Timer timer;
         public WriteToFileHostedService(IHostEnvironment environment) 
         {
             this.environment = environment;
         }
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            WriteToFile("WriteToFileHostedService : Process Started " + DateTime.Now.ToString("H:mm:ss"));
+            WriteToFile("WriteToFileHostedService : Process Started " + DateTime.Now.ToString("dd/MM/yyyy H:mm:ss"));
+            timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
             return Task.CompletedTask;
         }
 
@@ -35,8 +36,20 @@ namespace IHostedServiceDemo.Services
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            WriteToFile("WriteToFileHostedService : Process Stopped " + DateTime.Now.ToString("H:mm:ss"));
+            WriteToFile("WriteToFileHostedService : Process Stopped " + DateTime.Now.ToString("dd/MM/yyyy H:mm:ss"));
+            /* Desactiva el timer sino es nulo cuando pare la tarea */
+            timer?.Change(Timeout.Infinite, 0);
             return Task.CompletedTask;
+        }
+
+        private void DoWork(object state) 
+        {
+            WriteToFile("WriteToFileHostedService : Doing some work at " + DateTime.Now.ToString("dd/MM/yyyy H:mm:ss"));
+        }
+        public void Dispose()
+        {
+            /* El signo ? asegura que solo se ejecuta Dispose si el timer no es nulo */
+            timer?.Dispose();
         }
     }
 }
